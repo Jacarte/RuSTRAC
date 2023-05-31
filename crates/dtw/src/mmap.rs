3b1cf7dca4dca9b92443845eb4c6a81bfb10a4b6
Javier Cabrera <xppcoder@gmail.com>
2023-05-31 21:26:23 +0200

    MMap

---
+++ crates/dtw/src/mmap.rs
@@ -0,0 +1,20 @@
+use crate::dtw::{Accesor, TokenID};
+use std::sync::Arc;
+use std::sync::Mutex;
+
+pub struct MMapWrapper {
+    pub size: usize,
+    pub ptr: Arc<Mutex<*mut u8>>,
+}
+
+impl Accesor for MMapWrapper {
+    fn get(&self, idx: usize) -> TokenID {
+        // get 8 bytes from the pointer
+        let ptr = unsafe { self.ptr.lock().unwrap().add(12 + idx * 8) };
+        return unsafe { *ptr } as TokenID;
+    }
+
+    fn size(&self) -> usize {
+        self.size
+    }
+}
